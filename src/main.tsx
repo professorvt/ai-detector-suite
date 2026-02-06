bc17977dbf94dad302922eff01e674247f5410d3
professorvt <profvtgaming@gmail.com>
2026-02-07 00:31:15 +0530

    feat: add Vercel Analytics and Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { BrowserRouter } from "react-router-dom"; // Ye line zaroori hai
 import { HelmetProvider } from "react-helmet-async";
 import App from "@/App.tsx";
 import "@/index.css";
+import { Analytics } from "@vercel/analytics/react";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-0GMQ5XSKPG");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
